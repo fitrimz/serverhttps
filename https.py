60e171ba553b6c5bbd3b08998107e69cf695c482
fitrimz <fitrimz99@gmail.com>
2021-06-12 17:15:32 -0400

    Https

---
+++ https.py
@@ -0,0 +1,25 @@
+import socketserver
+import http.server
+import ssl
+
+from http.server import HTTPServer, BaseHTTPRequestHandler 
+
+class pelayan(BaseHTTPRequestHandler):
+
+	def do_GET(self):
+		if self.path == '/':
+			self.path = 'index.html'
+		try:
+			file_to_open = open(self.path[1:]).read()
+			self.send_response(200)
+		except:
+			file_to_open = "File NOT Found!!"
+			self.send_response(404)
+		self.end_headers()
+		self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+
+pelayan = socketserver.TCPServer(('0.0.0.0', 10000), http.server.SimpleHTTPRequestHandler)
+
+pelayan.socket = ssl.wrap_socket(pelayan.socket, certfile= "/home/nial/httpsServer/cert.pem", keyfile= "/home/nial/httpsServer/key.pem", server_side=True)
+pelayan.serve_forever()
